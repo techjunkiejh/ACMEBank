b52dd85e1792a0139f33e821ff02c1eb82d4a461
Jason Huynh <tech.junkie.jh@gmail.com>
2019-09-02 15:30:28 -0400

    Express JS Setup

--- app/js/controllers/bankController.js
+++ app/js/controllers/bankController.js
@@ -1,6 +1,9 @@
-﻿'use strict';
+﻿(function () {
+    'use strict';
 
-angular.module('bankController', [])
-    .controller('bankController', ["$scope", function ($scope) {
-        $scope.bankAppName = "ACME Bank";
-    }]);
+    angular.module('bankController', [])
+        .controller('bankController', ["$scope", function ($scope) {
+            $scope.bankAppName = "My Bank Application";
+        }]);
+
+})();
